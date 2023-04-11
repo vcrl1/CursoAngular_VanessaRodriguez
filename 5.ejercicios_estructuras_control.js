724dbaf0a8b3f43674a4e69dd4f2820b4007da35
vcrl1 <vcrl1994@gmail.com>
2023-04-11 11:40:46 +0200

    Cambio de nombre de punto 4 y enunciados punto 5

---
+++ 5.ejercicios_estructuras_control.js
@@ -0,0 +1,33 @@
+/*Uso de la estructura if-else
+1. Crea una variable numero y asígnale un número entero.
+2. Escribe una estructura if-else que muestre en consola si el número es positivo, negativo o cero.
+/*
+
+
+/*Uso de la estructura switch
+1. Crea una variable fruta y asígnale el nombre de una fruta.
+2. Utiliza una estructura switch para determinar si la fruta es roja (manzana, fresa), amarilla
+(plátano, piña) o de otro color (naranja, uva) y muestra el resultado en consola.
+/*
+
+
+
+/*Uso de bucle for
+1. Escribe un bucle for que muestre en consola los números del 1 al 10
+/*
+
+
+/* Uso de bucle while
+1. Escribe un bucle while que muestre en consola los números del 1 al 10
+/*
+
+
+
+/*Uso de bucle do-while
+1. Escribe un bucle do-while que muestre en consola los números del 1 al 10.
+/*
+
+
+/*Uso de bucle for anidado
+1. Escribe un bucle for anidado que muestre en consola una tabla de multiplicar del 1 al 10.
+/*
