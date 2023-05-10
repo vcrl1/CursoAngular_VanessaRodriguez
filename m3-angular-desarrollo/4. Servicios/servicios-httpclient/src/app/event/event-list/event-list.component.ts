71d0558095e5642cff275646aaba3252e7d196b0
vcrl1 <vcrl1994@gmail.com>
2023-05-10 10:45:23 +0200

    Eventos en proyecto "servicios-http..."  findAll, findByCompletedTrue, findByCompletedFalse

--- m3-angular-desarrollo/4. Servicios/servicios-httpclient/src/app/event/event-list/event-list.component.ts
+++ m3-angular-desarrollo/4. Servicios/servicios-httpclient/src/app/event/event-list/event-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../event.service';
 import { IEvent } from '../event.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-event-list',
@@ -10,16 +11,26 @@ import { IEvent } from '../event.model';
 export class EventListComponent implements OnInit {
   events: IEvent[] = []
   constructor(private eventService: EventService) { }
-//interfaz de angular para arrancar el componente
+  //interfaz de angular para arrancar el componente
   ngOnInit(): void {
     // this.eventService.findAll().subscribe(data => console.log(data));
+    this.findAll()
+  }
+
+  findAll():void{
     this.eventService.findAll().subscribe(data => {
       console.log(data);
       this.events = data;
-    })
+    })  }
 
+
+  findByCompletedTrue():void {
+    this.eventService.findAllByCompletedTrue().subscribe(data => this.events = data)
   }
 
-  
+  findByCompletedFalse():void{
+    this.eventService.findAllByCompletedFalse().subscribe(data => this.events = data)
+  }
+
 
 }
--- m3-angular-desarrollo/4. Servicios/servicios-httpclient/src/app/event/event.service.ts
+++ m3-angular-desarrollo/4. Servicios/servicios-httpclient/src/app/event/event.service.ts
@@ -14,4 +14,19 @@ export class EventService {
   findById(id: number): Observable<any> {
     return this.httpClient.get(`${this.url}/${id}`)
   }
+
+  //Con un solo parámetro: ? completed=true
+  //Con dos parámetros: ?param1=value1&param2=value2
+  findAllByCompletedTrue(): Observable<any> {
+    return this.httpClient.get(`${this.url}?completed=true`)
+  }
+  findAllByCompletedFalse(): Observable<any> {
+    return this.httpClient.get(`${this.url}?completed=false`)
+  }
+
+
+  //findAllByCompletedFalse
+  //save
+  //update
+  //deleteById
 }
