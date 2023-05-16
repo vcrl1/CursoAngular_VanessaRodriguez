92cd732da56420af12e7f2ef8657d3cf53cf9be3
vcrl1 <vcrl1994@gmail.com>
2023-05-16 12:13:15 +0200

    Proyecto 7. Fin

--- m3-angular-desarrollo/4. Servicios/proyecto7/src/app/users/user-list/user-list.component.ts
+++ m3-angular-desarrollo/4. Servicios/proyecto7/src/app/users/user-list/user-list.component.ts
@@ -10,16 +10,50 @@ import { IUser } from '../user.model';
 
 export class UserListComponent implements OnInit {
   users: IUser[] = []
+  error:boolean=false
   constructor(private userService: UserService) { }
   ngOnInit(): void {
 
-    this.userService.findAll().subscribe(data => {
-      console.log(data);
-      console.log(data.status);
-      console.log(data.body);
-      this.users = data.body ?? [];
+//Opción 1 : función next simple:
+//this.userService.findAll().subscribe( data=> this.users=data.body ??[])
+
+//Opción 2: función next bloque{}
+  //   this.userService.findAll().subscribe(data => {
+  //     console.log(data);
+  //     console.log(data.status);
+  //     console.log(data.body);
+  //     this.users = data.body ?? [];
+  //   })
+  // }
+
+  /* 
+  
+//Opción 3: CON FUNCIONES 
+    --NEXT obligatorio 
+    --ERROR y COMPLETE opcional
+
+    this.userService.findAll().subscribe(
+    next: (data: HttpResponse<IUser[]>) => this.users=data.body ??[],
+    error: (err:Error)=> console.error("Se ha producido un error"), 
+    complete:()=> console.log('findAll completado')
+    }) 
+
+//Opción 4: funciones con bloques 
+this.userService.findAll().subscribe({
+    next: (data: HttpResponse<IUser[]>) =>{console.log(data.status),
+    this.users=data.body ??[]
+    },
+    error: (err:Error)=> {console.error('Error: '+err.message), 
+    this.error=true;
+    },
+    complete:()=> console.log('findAll completado')
     })
   }
 
+showError(err:Error){
+  this.error=err.message;
+  setTimeout(()=>this.error=" ", 5000)}
+    */
 
+}
 }
