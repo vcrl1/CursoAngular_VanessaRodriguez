9f8835492c8da234ba4536ad80f5600e542eefbf
vcrl1 <vcrl1994@gmail.com>
2023-05-25 12:34:07 +0200

    Angular, selector único. Proyecto 15

--- m3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form/user-form.component.ts
+++ m3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form/user-form.component.ts
@@ -15,16 +15,25 @@ export class UserFormComponent {
     salary: new FormControl(''),    
     isStudent: new FormControl(false),
     birthday: new FormControl(''),    
+    entryHour: new FormControl(''),
+    bookingDate: new FormControl(''),
+    jobCategory: new FormControl(''),
+    company: new FormControl(''),
 
   });
 
   save() {
+    // Se mandaría a un servicio
     console.log('Formulario enviado:', this.userForm.get('email')?.value);
     console.log('Formulario enviado:', this.userForm.get('fullName')?.value);
     console.log('Formulario enviado:', typeof this.userForm.get('age')?.value);
     console.log('Formulario enviado:', this.userForm.get('salary')?.value);
     console.log('Formulario enviado:', this.userForm.get('isStudent')?.value);
     console.log('Formulario enviado:', this.userForm.get('birthday')?.value);
+    console.log('Formulario enviado:', this.userForm.get('entryHour')?.value);
+    console.log('Formulario enviado:', this.userForm.get('bookingDate')?.value);
+    console.log('Formulario enviado:', this.userForm.get('jobCategory')?.value);
+    console.log('Formulario enviado:', this.userForm.get('company')?.value);
 
     this.userForm.reset()
   }
