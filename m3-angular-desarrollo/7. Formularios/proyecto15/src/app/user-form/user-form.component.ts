e20d4d0404edc1effa03a261239a80b8143ef330
vcrl1 <vcrl1994@gmail.com>
2023-05-25 14:07:06 +0200

    PRoyecto 15. Imagen en formulario y su previsualización.

--- m3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form/user-form.component.ts
+++ m3-angular-desarrollo/7. Formularios/proyecto15/src/app/user-form/user-form.component.ts
@@ -6,19 +6,22 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
+export class UserFormComponent{ 
 
   userForm = new FormGroup({
     email: new FormControl(''),
     fullName: new FormControl(''),
     age: new FormControl(''),
-    salary: new FormControl(''),    
+    salary: new FormControl(''),
     isStudent: new FormControl(false),
-    birthday: new FormControl(''),    
+    birthday: new FormControl(''),
     entryHour: new FormControl(''),
     bookingDate: new FormControl(''),
     jobCategory: new FormControl(''),
     company: new FormControl(''),
+    hobbies: new FormControl(''),
+    bio: new FormControl(''),
+    avatar: new FormControl(''),
 
   });
 
@@ -34,9 +37,24 @@ export class UserFormComponent {
     console.log('Formulario enviado:', this.userForm.get('bookingDate')?.value);
     console.log('Formulario enviado:', this.userForm.get('jobCategory')?.value);
     console.log('Formulario enviado:', this.userForm.get('company')?.value);
+    console.log('Formulario enviado:', this.userForm.get('hobbies')?.value);
+    console.log('Formulario enviado:', this.userForm.get('bio')?.value);
+    console.log('Formulario enviado:', this.userForm.get('avatar')?.value);
 
     this.userForm.reset()
   }
 
+  imageSrc: string | undefined
+  uploadFile(event: Event): void {       //Con esto puedo acceder a los archivos.
+    let target = event.target as HTMLInputElement
+    if (target.files !== null && target.files.length > 0) {
+      let fileImg = target.files[0];
 
+      //OPCIONAL para mostrar al usuario la imagen 
+      let reader = new FileReader();
+      reader.onload = ev => this.imageSrc = reader.result as string //qué hacer cuando se lea la imagen 
+      reader.readAsDataURL(fileImg); //leer la imagen
+
+    }
+  }
 }
