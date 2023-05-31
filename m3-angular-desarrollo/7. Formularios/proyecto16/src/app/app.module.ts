903f44fc1ee1ece374aee1e8233325481a1c71e0
vcrl1 <vcrl1994@gmail.com>
2023-05-31 11:29:58 +0200

    Proyecto 16. Formularios

--- m3-angular-desarrollo/7. Formularios/proyecto16/src/app/app.module.ts
+++ m3-angular-desarrollo/7. Formularios/proyecto16/src/app/app.module.ts
@@ -4,17 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserFormComponent } from './user-form/user-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent
+    UserFormComponent,
+
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule, 
+    MatRadioModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
   ],
-  providers: [],
+  providers: [{
+    provide:MAT_DATE_LOCALE, useValue:'es-ES'}], //Para darle formato España al calendario.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- m3-angular-desarrollo/7. Formularios/proyecto16/src/app/user-form/user-form.component.ts
+++ m3-angular-desarrollo/7. Formularios/proyecto16/src/app/user-form/user-form.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'; //Importamos 
+
+interface ICompany {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
 
+export class UserFormComponent {
+  companies: ICompany[] = [
+    { id: 1, name: "company1" },
+    { id: 2, name: "company2" },
+    { id: 3, name: "company3" },
+    { id: 4, name: "company4" },
+  ]
+  hobbies: string[] = ['sports', 'music', 'travel']
+
+  hide = true;
+
+  minBirthday = new Date(1910,0,1) //Enero empieza en 0.
+  maxBirthday = new Date(2005,0,1)
+
+
+  userForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    bio: new FormControl('', [Validators.required, Validators.maxLength(256)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    isStudent: new FormControl(false),
+    acceptConditions: new FormControl(false, [Validators.requiredTrue]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9$%&/()]{8,20}$')]),
+    salary: new FormControl('', [Validators.required, Validators.min(10000), Validators.max(200000)]),
+    department: new FormControl('', [Validators.required]),
+    company: new FormControl('', [Validators.required]),
+    hobbies: new FormControl('', [Validators.required]),
+    city: new FormControl(''),
+    postalCode: new FormControl('', [Validators.pattern('^[0-9]{5}$'), Validators.max(99999), Validators.min(0)]),
+    birthday: new FormControl(null),
+    jobStartDate: new FormControl(null),
+    jobEndDate: new FormControl(null),
+
+  })
+
+  save() { }
 }
