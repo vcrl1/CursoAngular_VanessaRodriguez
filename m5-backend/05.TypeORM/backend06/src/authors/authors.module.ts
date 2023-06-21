88934f5edf60839f73b8691840f369ab4e85a3a0
vcrl1 <vcrl1994@gmail.com>
2023-06-21 14:18:56 +0200

    configuración para que cree las tablas book y author

--- m5-backend/05.TypeORM/backend06/src/authors/authors.module.ts
+++ m5-backend/05.TypeORM/backend06/src/authors/authors.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Author } from './authors.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Author])], //Tabla Author 
   controllers: [AuthorsController],
   providers: [AuthorsService]
 })
-export class AuthorsModule {}
+export class AuthorsModule { }
