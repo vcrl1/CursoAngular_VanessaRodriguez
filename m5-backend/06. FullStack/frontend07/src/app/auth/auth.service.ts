bdee2ba8f2899ad7b6899efbf732ba2a8d03b560
vcrl1 <vcrl1994@gmail.com>
2023-07-04 22:44:02 +0200

    FullStack angular AuthService

--- m5-backend/06. FullStack/frontend07/src/app/auth/auth.service.ts
+++ m5-backend/06. FullStack/frontend07/src/app/auth/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+
+  url: string = "http://localhost:3000/auth";
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(login: any): Observable<any> {
+    return this.httpClient.post(`${this.url}/login`, login);
+  }
+
+  register(user: any): Observable<any> {
+    return this.httpClient.post(`${this.url}/register`, user);
+  }
+
+  // ... más comprobaciones ...
+  // comprobar un rol
+  // comprobar si ha iniciado sesión
+  // logout
+
 }
