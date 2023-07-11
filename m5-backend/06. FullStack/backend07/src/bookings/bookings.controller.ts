6a9d77574ec124440375cd3ef21fb6e7817d4841
vcrl1 <vcrl1994@gmail.com>
2023-07-12 00:16:08 +0200

    FullStack backend BookingsController create() cargar usuario en la reserva

--- m5-backend/06. FullStack/backend07/src/bookings/bookings.controller.ts
+++ m5-backend/06. FullStack/backend07/src/bookings/bookings.controller.ts
@@ -1,21 +1,26 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Request, UseGuards, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { Booking } from './bookings.model';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('bookings')
 export class BookingsController {
 
-    constructor(private bookingService: BookingsService) {}
-
+    constructor(private bookingService: BookingsService) { }
+    @UseGuards(AuthGuard('jwt'))
     @Get('user/:userId')
     findAllByUserId(
-        @Param("userId", ParseIntPipe) 
+        @Param("userId", ParseIntPipe)
         userId: number): Promise<Booking[]> {
         return this.bookingService.findAllByUserId(userId);
     }
-
+    @UseGuards(AuthGuard('jwt'))
     @Post()
-    async create(@Body() booking: Booking): Promise<Booking> {
+    async create(
+        @Request() request,
+        @Body() booking: Booking): Promise<Booking> {
+        console.log(request.user);
+        booking.user = request.user;
         return await this.bookingService.create(booking);
     }
 
